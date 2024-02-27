a14015aaeb92dafa175f1cf29e8d35929eb9e54c
Major-Scott <99700517+Major-Scott@users.noreply.github.com>
2024-02-26 20:37:31 -0800

    Fixed folder location issue

--- LC_Soundpack/Plugin.cs
+++ LC_Soundpack/Plugin.cs
@@ -14,9 +14,9 @@ namespace LC_Soundpack
     [BepInPlugin(modGUID, modName, modVersion)]
     public class Plugin : BaseUnityPlugin
     {
-        private const string modGUID = "Slam.SlamsBonanzaSoundPack";
+        private const string modGUID = "Slam.Slams_Bonanza_SoundPack";
         private const string modName = "Slam's Bonanza Sound Pack";
-        private const string modVersion = "0.1.1";
+        private const string modVersion = "0.1.2";
 
         private readonly Harmony harmony = new Harmony(modGUID);
         private static Plugin instance;
@@ -29,19 +29,19 @@ namespace LC_Soundpack
             mls = BepInEx.Logging.Logger.CreateLogSource(nameof(modGUID));
             mls.LogInfo("Slam's Bonanza Sound Pack!");
 
-            AudioClip JackInTheBoxTheme = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "JackInTheBoxTheme.mp3");
-            AudioClip Pop1 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "Pop1.mp3");            
-            AudioClip Chitter1 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "Chitter1.wav");
-            AudioClip Chitter2 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "Chitter2.wav");
-            AudioClip Chitter3 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "Chitter3.wav");
-            AudioClip AngryScreech = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "AngryScreech.wav");
-            AudioClip AngryScreech2 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "AngryScreech2.wav");
-            AudioClip BugDie = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "BugDie.wav");
-            AudioClip ShovelHitDefault = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "ShovelHitDefault.wav");
-            AudioClip ShovelHitDefault2 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "ShovelHitDefault2.wav");
-            AudioClip DropMetalObject3 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "DropMetalObject3.wav");
-            AudioClip AirHorn1 = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "AirHorn1.wav");
-            AudioClip AirHornFar = SoundTool.GetAudioClip("Slam-SlamsBonanzaSoundPack", "AirHornFar.wav");
+            AudioClip JackInTheBoxTheme = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "JackInTheBoxTheme.mp3");
+            AudioClip Pop1 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "Pop1.mp3");            
+            AudioClip Chitter1 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "Chitter1.wav");
+            AudioClip Chitter2 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "Chitter2.wav");
+            AudioClip Chitter3 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "Chitter3.wav");
+            AudioClip AngryScreech = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "AngryScreech.wav");
+            AudioClip AngryScreech2 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "AngryScreech2.wav");
+            AudioClip BugDie = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "BugDie.wav");
+            AudioClip ShovelHitDefault = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "ShovelHitDefault.wav");
+            AudioClip ShovelHitDefault2 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "ShovelHitDefault2.wav");
+            AudioClip DropMetalObject3 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "DropMetalObject3.wav");
+            AudioClip AirHorn1 = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "AirHorn1.wav");
+            AudioClip AirHornFar = SoundTool.GetAudioClip("Slam-Slams_Bonanza_SoundPack", "AirHornFar.wav");
 
             SoundTool.ReplaceAudioClip("JackInTheBoxTheme", JackInTheBoxTheme);
             SoundTool.ReplaceAudioClip("Pop1", Pop1);
